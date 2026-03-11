1f570b9b15388e8c0b0d835d3c62c61f56879566
Jan Rzepka <j.a.rzepka@student.tue.nl>
2026-03-11 15:04:55 +0100

    Calibrate single saved frame

--- VirtualEnviroment.py
+++ VirtualEnviroment.py
@@ -1,5 +1,7 @@
 import pyvista as pv
 import numpy as np 
+from Calibration import *
+import copy
 
 class VirtualEnviroment:
     def __init__(self):
@@ -11,14 +13,42 @@ class VirtualEnviroment:
         self.cameras_intrinsics = cameras_intrinsics # (camera_count, 3, 3)
 
     def add_frame_for_calibration(self, true_pos, screen_pos, camera_index):
-        self.calibration_info[camera_index].append((true_pos, screen_pos))
+        a, b = copy.deepcopy(true_pos), copy.deepcopy(screen_pos)
+        self.calibration_info[camera_index].append((a, b))
+        print(f"Added frame {len(self.calibration_info[camera_index])} for camera {camera_index}.")
 
     def calibrate_single_frame(self, true_pos, screen_pos, camera_index):
         """
             Calibrating based on info from a single frame
         """
+        camera_intrinsics = self.cameras_intrinsics[camera_index]
+        print(camera_intrinsics)
+        solved_pnp, best_combo, best_error = find_camera_position_and_rotation_from_3_fixed_balls(
+            true_positions=true_pos,
+            video_positions=screen_pos,
+            camera_intrinsics=camera_intrinsics)
+        
+        if solved_pnp is None:
+            print("Could not solve PnP with the given points.")
+            return None
+        
+        camera_position, camera_rotation = rvec_tvec_to_camera_pose(solved_pnp[0], solved_pnp[1])
+        #print(f"Best combo: {best_combo}, Reprojection error: {best_error}")
+
+        return camera_position, camera_rotation
 
     def calibrate_camera(self, camera_index):
         """
             Find the best position based on info from many frames
         """
+
+        print(f"------------Calibrating camera {camera_index+1}------------")
+
+        last_frame = self.calibration_info[camera_index][-1]
+
+        true_pos, screen_pos = last_frame
+        res = self.calibrate_single_frame(true_pos, screen_pos, camera_index)
+
+        if res is not None:
+            print(f"Camera position: {res[0]}")
+
