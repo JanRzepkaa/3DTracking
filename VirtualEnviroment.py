fffdebd4409137d4f2193d170b644186667a97bd
Jan Rzepka <j.a.rzepka@student.tue.nl>
2026-03-11 16:00:50 +0100

    Solve PnP for each newly saved frame and sort points

--- VirtualEnviroment.py
+++ VirtualEnviroment.py
@@ -9,12 +9,13 @@ class VirtualEnviroment:
 
     def initialize_calibration(self, camera_count, cameras_intrinsics):
         self.calibration_info = [[] for i in range(camera_count)]
+        self.already_solved_frames = [[] for i in range(camera_count)]
         # Single entry = (true_position, screen_position)
         self.cameras_intrinsics = cameras_intrinsics # (camera_count, 3, 3)
 
     def add_frame_for_calibration(self, true_pos, screen_pos, camera_index):
         a, b = copy.deepcopy(true_pos), copy.deepcopy(screen_pos)
-        self.calibration_info[camera_index].append((a, b))
+        self.calibration_info[camera_index].append([a, b])
         print(f"Added frame {len(self.calibration_info[camera_index])} for camera {camera_index}.")
 
     def calibrate_single_frame(self, true_pos, screen_pos, camera_index):
@@ -22,7 +23,6 @@ class VirtualEnviroment:
             Calibrating based on info from a single frame
         """
         camera_intrinsics = self.cameras_intrinsics[camera_index]
-        print(camera_intrinsics)
         solved_pnp, best_combo, best_error = find_camera_position_and_rotation_from_3_fixed_balls(
             true_positions=true_pos,
             video_positions=screen_pos,
@@ -32,10 +32,16 @@ class VirtualEnviroment:
             print("Could not solve PnP with the given points.")
             return None
         
+        sorted_screen_pos = []
+
+        for i in range(len(best_combo)):
+            j = best_combo[i]
+            sorted_screen_pos.append(screen_pos[j])
+        
         camera_position, camera_rotation = rvec_tvec_to_camera_pose(solved_pnp[0], solved_pnp[1])
         #print(f"Best combo: {best_combo}, Reprojection error: {best_error}")
 
-        return camera_position, camera_rotation
+        return camera_position, camera_rotation, sorted_screen_pos
 
     def calibrate_camera(self, camera_index):
         """
@@ -44,11 +50,23 @@ class VirtualEnviroment:
 
         print(f"------------Calibrating camera {camera_index+1}------------")
 
-        last_frame = self.calibration_info[camera_index][-1]
+        already_solved_count = len(self.already_solved_frames[camera_index])
+        all_frames_count = len(self.calibration_info[camera_index])
+        for i in range(already_solved_count, all_frames_count):
+            i_frame = self.calibration_info[camera_index][i]
+            i_true_pos, i_screen_pos = i_frame
 
-        true_pos, screen_pos = last_frame
-        res = self.calibrate_single_frame(true_pos, screen_pos, camera_index)
+            if len(i_true_pos) != len(i_screen_pos):
+                res = None
+            else:
+                res = self.calibrate_single_frame(i_true_pos, i_screen_pos, camera_index)
 
-        if res is not None:
-            print(f"Camera position: {res[0]}")
+            if res is not None:
+                print(res[0])
+                self.calibration_info[camera_index][i][1] = res[2]
+
+                self.already_solved_frames[camera_index].append((res[0], res[1]))
+            else:
+                self.already_solved_frames[camera_index].append(None)
+
 
