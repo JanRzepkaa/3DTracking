9591c2edecdd5e39c7cfbcb93e4ad042e60d0ff4
Jan Rzepka <j.a.rzepka@student.tue.nl>
2026-03-18 16:30:36 +0100

    Added function for virtual enviroment

--- PyVistaTracker.py
+++ PyVistaTracker.py
@@ -10,10 +10,12 @@ class PyVistaTracker():
     def __init__(self, cameras_info, actors):
         self.cameras_info = cameras_info
         self.camera_count = len(cameras_info)
-        self.actors = actors
+        self.actors = actors # {"group_name_1" : [], "group_name_2": []}
 
         self.initialize_plotters()
         self.initialize_virtual_env()
+
+        self.video_analyzer = AnalyzePyVistaVideo(False)
 
 
     def initialize_plotters(self):
@@ -21,17 +23,18 @@ class PyVistaTracker():
 
         for i in range(self.camera_count):
             cam = self.cameras_info[i]
-            new_plotter = pv.Plotter(off_screen=True, window_size=cam.resolution)
-            new_plotter.camera.position = cam.position
-            new_plotter.camera.focal_point = cam.focal_point
+            new_plotter = pv.Plotter(off_screen=True, window_size=cam["resolution"])
+            new_plotter.camera.position = cam["position"]
+            new_plotter.camera.focal_point = cam["focal_point"]
             new_plotter.camera.up = (0, 0, 1)
             new_plotter.camera.clipping_range = (0.6, 1000)
 
-            for actor in self.actors:
-                new_plotter.add_actor(actor)
+            for actors_group in self.actors.values():
+                for actor in actors_group:
+                    new_plotter.add_actor(actor)
 
             my_light = pv.Light(
-                position=cam.position, 
+                position=cam["resolution"], 
                 focal_point=(0, 0, 0), 
                 color='white',
                 light_type='scene light' # 'scene light' means it stays fixed in the 3D world
@@ -64,4 +67,47 @@ class PyVistaTracker():
             all_cameras_intristic.append(intrinsics)
         
         self.virtual_env.initialize_calibration(self.camera_count, all_cameras_intristic)
+
+    def fake_calibration(self):
+        camera_positions = []
+        for cam in self.cameras_info:
+            camera_positions.append(cam["position"])
+        self.virtual_env.fake_calibration(camera_positions)
+
+
+    def get_raw_view_from_camera(self, camera_index):
+        img_rgb = self.plotters[camera_index].screenshot(None, return_img=True)
+        return img_rgb
+    def get_bgr_view_from_camera(self, camera_index):
+        img_rgb = self.get_raw_view_from_camera(camera_index)
+        img_bgr = img_rgb[:, :, ::-1].copy()
+        return img_bgr
+
+    def get_position_of_actors_in_group(self, group_name):
+        positions = []
+        for actor in self.actors[group_name]:
+            pos = actor.GetPosition()
+            positions.append(pos)
+        return positions
+
+    def add_frame_for_calibration(self, camera_index, group_name):
+        img_bgr = self.get_bgr_view_from_camera(camera_index)
+        _, screen_positon = self.video_analyzer.find_centroids_and_contours(img_bgr)
+        all_true_positions = self.get_position_of_actors_in_group(group_name)
+
+        self.virtual_env.add_frame_for_calibration(all_true_positions, screen_positon, camera_index)
+
+    def calibrate_virtual_cameras(self):
+        for i in range(self.camera_count):
+            self.virtual_env.calibrate_camera(i)
+
+    def track_player(self):
+        screen_positions = [None for i in range(self.camera_count)]
+        for camera_index in range(self.camera_count):
+            img_bgr = self.get_bgr_view_from_camera(camera_index)
+            _, pos = self.video_analyzer.find_centroids_and_contours(frame=img_bgr.copy(), color="lime")
+            screen_positions[i] = pos if len(pos)>0 else None
+
+        for i in range(self.camera_count):
+            self.virtual_env.add_lines_to_all_points(i, screen_positions[i])
 
